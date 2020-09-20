1c3ad85120db065856cdd03c1e583ba12edb20ef
SalvatoreCognetta <salvatore.cognetta@gmail.com>
2020-09-20 18:50:16 +0200

    Added control-movement association.

--- js/main/constants.js
+++ js/main/constants.js
@@ -3,6 +3,10 @@ var engine;
 var camera;
 
 var framerate = 60;
-var down = new BABYLON.Vector3(0,-.981,0);
+var down = new BABYLON.Vector3(0,-.8,0);
 
 var torsoRotation;
+
+var rotationToWalkEnded = false;
+
+var direction = 1; // Direction of the hero (1: right, -1: left)
